a2c67d23a71333ac3dd013eac3ef3da24a112fe5
Anurag Atul Srivastava <srivastavaanurag111222@gmail.com>
2025-11-24 09:27:10 +0530

    Add files via upload

---
+++ Business Problems Netflix.sql
@@ -0,0 +1,31 @@
+-- 20 Business Problems & Solutions
+
+1. Count the number of Movies vs TV Shows.
+2. Number of movies released per year.
+3. Top 10 directors by number of titles.
+4. Find the most common rating for movies and TV shows.
+5. List all movies released in a specific year (e.g., 2020).
+6. Find the top 5 countries with the most content on Netflix.
+7. What percentage of content is family/kids oriented?
+8. Identify the longest movie or TV show duration.
+9. Find content added in the last 5 years.
+10. Find all the movies/TV shows by director 'Rajiv Chilaka'!
+11. List all TV shows with more than 5 seasons.
+12. Count the number of content items in each genre.
+13. What is the share of Originals vs Non-Originals (approx. by directors or lack thereof)?
+14. Find each year and the average numbers of content release by India on netflix,return top 5 year with highest avg content release!
+15. List all movies that are documentaries.
+16. Find all content without a director.
+17. Find how many movies actor 'Salman Khan' appeared in last 10 years!
+18. Find the top 10 actors who have appeared in the highest number of movies produced in India.
+19. Categorize the content based on the presence of the keywords 'kill' and 'violence' in the description field.
+    Label content containing these keywords as 'Bad' and all other content as 'Good'. Count how many items fall into each category.
+20. Does Netflix focus more on recent releases or older content?
+
+
+
+
+
+
+
+
